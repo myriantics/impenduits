26a3cb7ddbcdc3f81300d6827b714ba8fb1e5e0c
Gavin <gthemaker032@gmail.com>
2025-01-25 22:51:53 -0800

    Reworked Impenduit Field contact abilities to treat player as if they're standing in rain.

--- src/main/java/net/myriantics/impenduits/mixin/EntityMixin.java
+++ src/main/java/net/myriantics/impenduits/mixin/EntityMixin.java
@@ -4,20 +4,16 @@ import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
-import net.myriantics.impenduits.ImpenduitsCommon;
 import net.myriantics.impenduits.blocks.ImpenduitFieldBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
-
-    @Shadow public abstract boolean isInSwimmingPose();
 
     @Unique
     public boolean isInsideImpenduitField;
@@ -26,35 +22,18 @@ public abstract class EntityMixin {
     private boolean interimIsInsideImpenduitField;
 
     @ModifyReturnValue(
-            method = "isTouchingWaterOrRain",
+            method = "isBeingRainedOn",
             at = @At(value = "RETURN")
     )
-    private boolean impenduitFieldOverride(boolean original) {
+    private boolean impenduits$isBeingRainedOnOverride(boolean original) {
         return original || isInsideImpenduitField;
-    }
-
-    @ModifyReturnValue(
-            method = "isWet",
-            at = @At(value = "RETURN")
-    )
-    private boolean fireExtinguishingOverride(boolean original) {
-        return original || isInsideImpenduitField;
-    }
-
-    @ModifyReturnValue(
-            method = "isTouchingWater",
-            at = @At(value = "RETURN")
-    )
-    private boolean swimmingOverride(boolean original) {
-        // you can swim through them because funny
-        return original || (isInsideImpenduitField && isInSwimmingPose());
     }
 
     @Inject(
             method = "checkBlockCollision",
             at = @At(value = "HEAD")
     )
-    private void initializeInterimVariable(CallbackInfo ci) {
+    private void impenduits$initializeInterimVariable(CallbackInfo ci) {
         interimIsInsideImpenduitField = false;
     }
 
@@ -62,7 +41,7 @@ public abstract class EntityMixin {
             method = "checkBlockCollision",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;onEntityCollision(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/Entity;)V")
     )
-    private void checkForImpenduitField(CallbackInfo ci, @Local BlockState checkedState) {
+    private void impenduits$checkForImpenduitField(CallbackInfo ci, @Local BlockState checkedState) {
         if (!interimIsInsideImpenduitField) {
             this.interimIsInsideImpenduitField = checkedState.getBlock() instanceof ImpenduitFieldBlock;
         }
@@ -72,7 +51,7 @@ public abstract class EntityMixin {
             method = "checkBlockCollision",
             at = @At(value = "TAIL")
     )
-    private void updateRealVariable(CallbackInfo ci) {
+    private void impenduits$updateIndicatorVariable(CallbackInfo ci) {
         if (isInsideImpenduitField != interimIsInsideImpenduitField) {
             this.isInsideImpenduitField = this.interimIsInsideImpenduitField;
         }
