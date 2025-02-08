d0a3a9ca507d485240806b72aa4ebab0aaac712a
thatsmartguy21 <thatsmartguy21@gmail.com>
2025-02-07 21:18:16 -0800

    Removed rain blocking bs because it was harder than I thought it'd be - might revisit later idk

--- src/main/java/net/myriantics/impenduits/ImpenduitsCommon.java
+++ src/main/java/net/myriantics/impenduits/ImpenduitsCommon.java
@@ -8,6 +8,7 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.piston.PistonBehavior;
